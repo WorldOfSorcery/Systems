317f941bc02798fc1991edbf496ee53d74551dc3
HektorTM <Paulvberg.hb@gmail.com>
2024-10-15 12:18:10 +0200

    Start on Global Stats

--- src/main/java/me/hektortm/woSSystems/stats/StatsManager.java
+++ src/main/java/me/hektortm/woSSystems/stats/StatsManager.java
@@ -1,8 +1,10 @@
 package me.hektortm.woSSystems.stats;
 
 import me.hektortm.woSSystems.WoSSystems;
+import me.hektortm.woSSystems.stats.utils.GlobalStat;
 import me.hektortm.woSSystems.stats.utils.Operation;
 import me.hektortm.woSSystems.stats.utils.Stat;
+import me.hektortm.wosCore.Utils;
 import me.hektortm.wosCore.WoSCore;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
@@ -19,7 +21,10 @@ import java.util.UUID;
 public class StatsManager {
 
     public File statsFolder = new File(WoSSystems.getPlugin(WoSSystems.class).getDataFolder(), "stats");
+    public File globalStatsFolder = new File(WoSSystems.getPlugin(WoSSystems.class).getDataFolder(), "globalstats");
+    public File globalStatsData = new File(WoSSystems.getPlugin(WoSSystems.class).getDataFolder(), "global_stats_data.yml");
     private final Map<String, Stat> stats = new HashMap<>();
+    private final Map<String, GlobalStat> globalStats = new HashMap<>();
 
     private final WoSCore core;
 
@@ -27,6 +32,7 @@ public class StatsManager {
         this.core = core;
 
         loadStats();
+        loadGlobalStats();
     }
 
     public void createStat(Stat stat) {
@@ -44,20 +50,62 @@ public class StatsManager {
         }
     }
 
-    public void addStat(CommandSender sender, String id, long max, boolean capped) {
-        if (stats.containsKey(id)) {
-            sender.sendMessage("Already exists");
-            return;
+    public void createGlobalStat(GlobalStat globalStat) {
+        File file = new File(globalStatsFolder, globalStat.getId().toLowerCase() + ".yml");
+        FileConfiguration config = YamlConfiguration.loadConfiguration(file);
+
+        config.set("id", globalStat.getId());
+        config.set("capped", globalStat.getCapped());
+        config.set("max", globalStat.getMax());
+
+        try {
+            config.save(file);
+        } catch (IOException e) {
+            Bukkit.getLogger().severe("Could not save global stat file for " + globalStat.getId());
         }
-
-        Stat stat = new Stat(id, max, capped);
-        stats.put(id, stat);
-
-        createStat(stat);
     }
 
-    public void deleteStat() {
+    public void addStat(CommandSender sender, String id, long max, boolean capped, boolean global) {
+        if (global) {
+            if (globalStats.containsKey(id)) {
+                sender.sendMessage("Already exists");
+                return;
+            }
 
+            GlobalStat stat = new GlobalStat(id.toLowerCase(), max, capped);
+            globalStats.put(id.toLowerCase(), stat);
+
+            createGlobalStat(stat);
+
+        } else {
+            if (stats.containsKey(id)) {
+                sender.sendMessage("Already exists");
+                return;
+            }
+
+            Stat stat = new Stat(id.toLowerCase(), max, capped);
+            stats.put(id.toLowerCase(), stat);
+
+            createStat(stat);
+        }
+    }
+
+    public void deleteStat(CommandSender sender, String id, boolean global) {
+        if (global) {
+            if (!globalStats.containsKey(id)) {
+                Utils.error(sender, "stats", "error.not-found");
+            }
+
+            File file = new File(globalStatsFolder, id + ".yml");
+            file.delete();
+        } else {
+            if (!stats.containsKey(id)) {
+                Utils.error(sender, "stats", "error.not-found");
+            }
+
+            File file = new File(statsFolder, id + ".yml");
+            file.delete();
+        }
     }
 
 
@@ -66,9 +114,9 @@ public class StatsManager {
         FileConfiguration playerData = core.getPlayerData(uuid, p.getName());
         File playerFile = new File(core.getDataFolder(), "playerdata" + File.separator + uuid + ".yml");
 
-        String path = "stats." + id;
+        String path = "stats." + id.toLowerCase();
 
-        Stat stat = stats.get(id);
+        Stat stat = stats.get(id.toLowerCase());
         long max = stat.getMax();
         int statAmount = playerData.getInt(path, 0);
         boolean capped = stat.getCapped();
@@ -77,7 +125,7 @@ public class StatsManager {
 
         switch (operation) {
             case GIVE:
-                if (isLimit(uuid, id, amount)) {
+                if (isLimit(uuid, id.toLowerCase(), amount)) {
                     playerData.set(path, max);
                     break;
                 }
@@ -105,6 +153,57 @@ public class StatsManager {
                 break;
         }
         core.savePlayerData(playerData, playerFile);
+    }
+
+    public void modifyGlobalStat(String id, long amount, Operation operation) {
+
+        File dataFile = globalStatsData;
+        FileConfiguration config = YamlConfiguration.loadConfiguration(dataFile);
+
+        String path = "globalstats." + id.toLowerCase();
+
+        GlobalStat stat = globalStats.get(id.toLowerCase());
+        long max = stat.getMax();
+        int statAmount = config.getInt(path, 0);
+        boolean capped = stat.getCapped();
+
+
+
+        switch (operation) {
+            case GIVE:
+                if (isGlobalLimit(id, amount)) {
+                    config.set(path, max);
+                    break;
+                }
+                config.set(path, statAmount + amount);
+                break;
+            case TAKE:
+                if ((statAmount - amount) < 0) {
+                    config.set(path, 0);
+                    break;
+                }
+                config.set(path, statAmount - amount);
+                break;
+            case SET:
+                if (capped) {
+                    if (amount > max) {
+                        config.set(path, max);
+                        break;
+                    }
+                }
+
+                config.set(path, amount);
+                break;
+            case RESET:
+                config.set(path, 0);
+                break;
+        }
+        try {
+            config.save(dataFile);
+        } catch (IOException e) {
+            Bukkit.getLogger().severe("Could not save Global stat value for: "+id);
+        }
+
     }
 
     public void reloadAllStats() {
@@ -150,11 +249,31 @@ public class StatsManager {
             }
         }
     }
+    public void loadGlobalStats() {
+        if(!globalStatsFolder.exists()) {
+            globalStatsFolder.mkdir();
+        }
 
+        File[] files = globalStatsFolder.listFiles((dir, name) -> name.endsWith(".yml"));
+        if(files != null) {
+            for (File file : files) {
+                FileConfiguration config = YamlConfiguration.loadConfiguration(file);
+                String id = config.getString("id");
+                long max = config.getInt("max");
+                boolean capped = config.getBoolean("capped");
 
+                GlobalStat stat = new GlobalStat(id, max, capped);
+                globalStats.put(id.toLowerCase(), stat);
+            }
+        }
+    }
 
     public Map<String, Stat> getStats() {
         return stats;
+    }
+
+    public Map<String, GlobalStat> getGlobalStats() {
+        return globalStats;
     }
 
     public boolean isLimit(UUID uuid, String id, long amount) {
@@ -164,7 +283,7 @@ public class StatsManager {
 
         long max = stat.getMax();
         boolean capped = stat.getCapped();
-        String path = "stats." + id;
+        String path = "stats." + id.toLowerCase();
         long pStatValue = playerData.getLong(path, 0);
 
         if (!capped) {
@@ -176,12 +295,38 @@ public class StatsManager {
 
     }
 
+    public boolean isGlobalLimit(String id, long amount) {
+        GlobalStat stat = globalStats.get(id.toLowerCase());
+        File dataFile = globalStatsData;
+        FileConfiguration config = YamlConfiguration.loadConfiguration(dataFile);
+
+        long max = stat.getMax();
+        boolean capped = stat.getCapped();
+        String path = "globalstats." + id.toLowerCase();
+        long statValue = config.getLong(path, 0);
+
+        if (!capped) {
+            return false;
+        }
+
+        long finalAmount = statValue + amount;
+        return finalAmount > max;
+    }
+
     public long getPlayerStat(UUID playerUUID, String statId) {
         File playerFile = new File(core.getDataFolder(), "playerdata" + File.separator + playerUUID.toString() + ".yml");
         FileConfiguration playerData = YamlConfiguration.loadConfiguration(playerFile);
 
         String path = "stats." + statId.toLowerCase();
         return playerData.getLong(path, 0);
+    }
+
+    public long getGlobalStat(String id) {
+        File dataFile = globalStatsData;
+        FileConfiguration config = YamlConfiguration.loadConfiguration(dataFile);
+
+        String path = "globalstats." + id.toLowerCase();
+        return config.getLong(path, 0);
     }
 
     public long getStatMax(String statId) {
--- src/main/java/me/hektortm/woSSystems/stats/commands/GlobalStatCommand.java
+++ src/main/java/me/hektortm/woSSystems/stats/commands/GlobalStatCommand.java
@@ -1,4 +1,23 @@
 package me.hektortm.woSSystems.stats.commands;
 
-public class GlobalStatCommand {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GlobalStatCommand implements CommandExecutor {
+
+    private final Map<String, StatsSubCommand> subCommand = new HashMap<>();
+
+    public GlobalStatCommand() {
+
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+        return false;
+    }
 }
