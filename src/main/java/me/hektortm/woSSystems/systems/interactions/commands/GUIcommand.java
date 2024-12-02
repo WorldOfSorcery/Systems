cfbea283eeb3a0a68cb9452edb2bd9b15262df78
HektorTM <Paulvberg.hb@gmail.com>
2024-12-02 19:30:37 +0100

    Start on GUI Command rework

--- src/main/java/me/hektortm/woSSystems/systems/interactions/commands/GUIcommand.java
+++ src/main/java/me/hektortm/woSSystems/systems/interactions/commands/GUIcommand.java
@@ -1,57 +1,29 @@
 package me.hektortm.woSSystems.systems.interactions.commands;
 
 
-import me.hektortm.woSSystems.systems.interactions.config.InteractionConfig;
+import me.hektortm.woSSystems.systems.interactions.commands.subcommands.gui.Open;
 import me.hektortm.woSSystems.systems.interactions.InteractionManager;
 import me.hektortm.woSSystems.systems.interactions.GUIManager;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
+import me.hektortm.woSSystems.utils.SubCommand;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class GUIcommand implements CommandExecutor {
-    private final GUIManager manager;
-    private final InteractionManager interactionManager;
+    private final Map<String, SubCommand> subCommands = new HashMap<>();
 
     public GUIcommand(GUIManager manager, InteractionManager interactionManager) {
-        this.manager = manager;
-        this.interactionManager = interactionManager;
+
+        subCommands.put("open", new Open(interactionManager, manager));
+
     }
 
     //TODO: Rework
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if (cmd.getName().equalsIgnoreCase("openGUI")) {
-            // Check if at least two arguments are provided
-            if (args.length < 2) {
-                sender.sendMessage(ChatColor.RED + "Usage: /opengui <player> <id>");
-                return true;
-            }
 
-            Player target = Bukkit.getPlayer(args[0]);
-            String interactionId = args[1];
-
-
-            if (target == null) {
-                sender.sendMessage(ChatColor.RED + "Player not found: " + args[0]);
-                return true;
-            }
-
-            InteractionConfig interactionConfig = interactionManager.getInteractionConfig(interactionId);
-
-            if (interactionConfig != null) {
-                Player p = Bukkit.getPlayer(args[0]);
-
-                p.closeInventory();
-                manager.openGUI(target, interactionConfig);
-                sender.sendMessage("Opening the GUI(" + interactionId+") for "+target.getName());
-            } else {
-                sender.sendMessage(ChatColor.RED + "GUI not found: " + interactionId);
-            }
-
-            return true;
-        }
         return false;
     }
 
---
+++ src/main/java/me/hektortm/woSSystems/systems/interactions/commands/subcommands/gui/Open.java
@@ -0,0 +1,63 @@
+package me.hektortm.woSSystems.systems.interactions.commands.subcommands.gui;
+
+import me.hektortm.woSSystems.systems.interactions.GUIManager;
+import me.hektortm.woSSystems.systems.interactions.InteractionManager;
+import me.hektortm.woSSystems.systems.interactions.config.InteractionConfig;
+import me.hektortm.woSSystems.utils.Permissions;
+import me.hektortm.woSSystems.utils.SubCommand;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class Open extends SubCommand {
+
+    private final InteractionManager interactionManager;
+    private final GUIManager guiManager;
+
+    public Open(InteractionManager interactionManager, GUIManager guiManager) {
+        this.interactionManager = interactionManager;
+        this.guiManager = guiManager;
+
+    }
+
+    @Override
+    public String getName() {
+        return "open";
+    }
+
+    @Override
+    public Permissions getPermission() {
+        return Permissions.GUI_OPEN;
+    }
+
+    @Override
+    public void execute(CommandSender sender, String[] args) {
+        // Check if at least two arguments are provided
+        if (args.length < 2) {
+            sender.sendMessage(ChatColor.RED + "Usage: /opengui <player> <id>");
+            return;
+        }
+
+        Player target = Bukkit.getPlayer(args[0]);
+        String interactionId = args[1];
+
+        if (target == null) {
+            sender.sendMessage(ChatColor.RED + "Player not found: " + args[0]);
+            return;
+        }
+
+        InteractionConfig interactionConfig = interactionManager.getInteractionConfig(interactionId);
+
+        if (interactionConfig != null) {
+            Player p = Bukkit.getPlayer(args[0]);
+
+            p.closeInventory();
+            guiManager.openGUI(target, interactionConfig);
+            sender.sendMessage("Opening the GUI(" + interactionId+") for "+target.getName());
+        } else {
+            sender.sendMessage(ChatColor.RED + "GUI not found: " + interactionId);
+        }
+
+    }
+}
