ace537b7c9469966469733ad71c2863a1a29455b
HektorTM <Paulvberg.hb@gmail.com>
2024-12-12 13:14:29 +0100

    Start on Nickname revamp

--- src/main/java/me/hektortm/woSSystems/chat/NicknameManager.java
+++ src/main/java/me/hektortm/woSSystems/chat/NicknameManager.java
@@ -1,10 +1,14 @@
 package me.hektortm.woSSystems.chat;
 
 import me.hektortm.wosCore.WoSCore;
+import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.List;
 import java.util.UUID;
 
 public class NicknameManager {
@@ -28,9 +32,50 @@ public class NicknameManager {
     }
 
     public void saveNickname(Player player, String nickname) {
+        FileConfiguration config = YamlConfiguration.loadConfiguration(currentNickFile);
+        
         UUID uuid = player.getUniqueId();
         String realName = player.getName();
 
+        // Update nickname and previous nicknames
+        config.set(uuid + ".username", realName);
+        config.set(uuid + ".nickname", nickname);
 
+        List<String> previousNicks = config.getStringList(uuid + ".previous_nicks");
+        previousNicks.add(nickname);
+        config.set(uuid + ".previous_nicks", previousNicks);
+
+        saveConfig();
     }
+
+    public void resetNickname(Player player) {
+        FileConfiguration config = YamlConfiguration.loadConfiguration(currentNickFile);
+        UUID uuid = player.getUniqueId();
+        config.set(uuid + ".nickname", null);  // Remove nickname
+        saveConfig();
+    }
+
+    public String getNickname(Player player) {
+        UUID uuid = player.getUniqueId();
+        return getNicknamesConfig().getString(uuid + ".nickname", player.getName());
+    }
+
+    public String getRealName(Player player) {
+        UUID uuid = player.getUniqueId();
+        return getNicknamesConfig().getString(uuid + ".username", player.getName());
+    }
+
+    private void saveConfig() {
+        try {
+            getNicknamesConfig().save(currentNickFile);
+        } catch (IOException e) {
+            Bukkit.getLogger().severe("Could not save nicknames.yml");
+        }
+    }
+
+    public FileConfiguration getNicknamesConfig() {
+        FileConfiguration config = YamlConfiguration.loadConfiguration(currentNickFile);
+        return config;
+    }
+    
 }
