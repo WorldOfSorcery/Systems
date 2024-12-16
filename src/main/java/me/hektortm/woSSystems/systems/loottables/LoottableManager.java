db8f48df937278865f95f2e627f6497812e53276
HektorTM <Paulvberg.hb@gmail.com>
2024-12-16 17:23:35 +0100

    Loottable structure

--- src/main/java/me/hektortm/woSSystems/systems/loottables/LoottableManager.java
+++ src/main/java/me/hektortm/woSSystems/systems/loottables/LoottableManager.java
@@ -3,9 +3,13 @@ package me.hektortm.woSSystems.systems.loottables;
 import me.hektortm.woSSystems.WoSSystems;
 import me.hektortm.woSSystems.systems.citems.CitemManager;
 import me.hektortm.woSSystems.systems.interactions.InteractionManager;
+import me.hektortm.woSSystems.utils.dataclasses.InteractionData;
+import me.hektortm.woSSystems.utils.dataclasses.LoottableData;
 import org.bukkit.entity.Player;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoottableManager {
 
@@ -13,6 +17,7 @@ public class LoottableManager {
     private final CitemManager citemManager;
     private final WoSSystems woSSystems = WoSSystems.getPlugin(WoSSystems.class);
     private final File loottableFolder = new File(woSSystems.getDataFolder(), "loottables");
+    public final Map<String, LoottableData> loottableMap = new HashMap<>();
 
     public LoottableManager(InteractionManager interManager, CitemManager citemManager) {
         this.interManager = interManager;
@@ -21,7 +26,6 @@ public class LoottableManager {
         if (!loottableFolder.exists()) {
             loottableFolder.mkdir();
         }
-
     }
 
     private void loadLoottables() {
