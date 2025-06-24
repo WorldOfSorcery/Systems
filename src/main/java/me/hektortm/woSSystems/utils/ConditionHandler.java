952a0a0a8640bc98b73eeb4d69d5f4674a4c6e2b
HektorTM <Paulvberg.hb@gmail.com>
2025-06-24 09:15:12 +0200

    Add new Conditions

--- src/main/java/me/hektortm/woSSystems/utils/ConditionHandler.java
+++ src/main/java/me/hektortm/woSSystems/utils/ConditionHandler.java
@@ -30,14 +30,9 @@ public class ConditionHandler {
 
 
     public boolean checkConditions(Player player, List<Condition> conditions) {
-        if (conditions == null || conditions.isEmpty()) {
-            return true;
-        }
-
+        if (conditions == null || conditions.isEmpty()) return true;
         for (Condition condition : conditions) {
-            if (!evaluate(player, condition)) {
-                return false; // if any condition fails, deny
-            }
+            if (!evaluate(player, condition)) return false;
         }
         return true;
     }
@@ -47,22 +42,20 @@ public class ConditionHandler {
             switch (condition.getName().toLowerCase()) {
                 case "has_citem":
                     return citems.hasCitemAmount(player, condition.getValue(), Integer.parseInt(condition.getParameter())); // Placeholder until i have a correct method for it
-
                 case "has_not_citem":
                     return !citems.hasCitemAmount(player, condition.getValue(), Integer.parseInt(condition.getParameter()));
-
                 case "has_unlockable":
                     return unlockables.hasPlayerUnlockable(player, condition.getValue());
-
                 case "has_not_unlockable":
                     return !unlockables.hasPlayerUnlockable(player, condition.getValue());
-
                 case "has_stats":
                     return stats.hasStatValue(player, condition.getValue(), Integer.parseInt(condition.getParameter()));
-
                 case "has_not_stats":
                     return !stats.hasStatValue(player, condition.getValue(), Integer.parseInt(condition.getParameter()));
-
+                case "server_has_globalstat":
+                    return hub.getStatsDAO().getGlobalStatValue(condition.getValue()) >= Long.parseLong(condition.getParameter());
+                case "server_has_not_globalstat":
+                    return hub.getStatsDAO().getGlobalStatValue(condition.getValue()) <= (condition.getParameter() != null ? Long.parseLong(condition.getParameter()) : 0);
                 case "is_in_region":
                     Map<UUID, String> playerRegions = plugin.getPlayerRegions();
                     String regionId = playerRegions.get(player.getUniqueId());
@@ -75,13 +68,26 @@ public class ConditionHandler {
                     return isCooldownActive(player, condition.getValue());
                 case "has_not_active_cooldown":
                     return !isCooldownActive(player, condition.getValue());
+                case "has_badge":
+                    return hub.getCosmeticsDAO().hasCosmetic(player.getUniqueId(), CosmeticType.BADGE, condition.getValue());
+                case "has_not_badge":
+                    return !hub.getCosmeticsDAO().hasCosmetic(player.getUniqueId(), CosmeticType.BADGE, condition.getValue());
+                case "has_prefix":
+                    return hub.getCosmeticsDAO().hasCosmetic(player.getUniqueId(), CosmeticType.PREFIX, condition.getValue());
+                case "has_not_prefix":
+                    return !hub.getCosmeticsDAO().hasCosmetic(player.getUniqueId(), CosmeticType.PREFIX, condition.getValue());
+                case "has_title":
+                    return hub.getCosmeticsDAO().hasCosmetic(player.getUniqueId(), CosmeticType.TITLE, condition.getValue());
+                case "has_not_title":
+                    return !hub.getCosmeticsDAO().hasCosmetic(player.getUniqueId(), CosmeticType.TITLE, condition.getValue());
+                case "has_currency":
+                    return hub.getEconomyDAO().getPlayerCurrency(player, condition.getValue()) >= (condition.getParameter() != null ? Long.parseLong(condition.getParameter()) : 0);
+                case "has_not_currency":
+                    return hub.getEconomyDAO().getPlayerCurrency(player, condition.getValue()) <= (condition.getParameter() != null ? Long.parseLong(condition.getParameter()) : 0);
                 case "permission":
                     return player.hasPermission(condition.getValue());
-
                 case "world":
                     return player.getWorld().getName().equalsIgnoreCase(condition.getValue());
-
-                // Add more condition types as needed
                 default:
                     return false;
             }
